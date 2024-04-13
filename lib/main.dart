bc634f4e1931461135add100aa93a7e6a82248a3
NalinNaik <nalinnaikmude@gmail.com>
2024-04-13 09:36:34 +0530

    Layout 1

--- lib/main.dart
+++ lib/main.dart
@@ -16,103 +16,38 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  List<String> hashtags = [
-    'photography',
-    'photooftheday',
-    'instagood',
-    'instadaily',
-    'picoftheday',
-    'beautiful',
-    'art',
-    'nature',
-    'travel',
-    'fashion',
-    'style',
-    'food',
-    'fitness',
-    'love',
-    'happy',
-    'smile',
-    'friends',
-    'selfie',
-    'summer',
-    'beach'
-  ];
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        // Your YouTube UI content goes here
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Consecteturer',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-              ),
-            ),
             Container(
+              color: Colors.grey,
+              child: Center(child: Text('A')),
+              height: MediaQuery.of(context).size.height/3.2,
               width: MediaQuery.of(context).size.width,
-              height: 500,
-              decoration: const BoxDecoration(
-                color: Colors.orange, // Choose your border color here
-              ),
             ),
-            const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    Icon(Icons.favorite_border),
-                    Icon(Icons.menu),
-                    Icon(Icons.attach_file),
-                  ],
-                )),
-            const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Divider(
-                  height: 1,
-                  color: Colors.black54,
-                )),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Row(
-                    children: [
-                      Icon(Icons.favorite_rounded),
-                      Text('Lorem Ipsum')
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const Text('lorem ipsum 321 pound sterling'),
-
-                  Wrap(
-                    children: [for(int a=0;a<hashtags.length;a++)
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          hashtags[a],
-                          style: const TextStyle(
-                            color: Colors.blue,
-                            // Set the color to blue
-                            fontSize: 24.0,
-                            // Set the font size
-                            fontWeight: FontWeight.bold,
-                            // Optional: Set the font weight
-                            fontStyle:
-                                FontStyle.italic, // Optional: Set the font style
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Container(
+                  color: Colors.grey,
+                  child: Center(child: Text('B')),
+                  height: MediaQuery.of(context).size.height/1.7,
+                  width: MediaQuery.of(context).size.width/1.5,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  color: Colors.grey,
+                  child: Center(child: Text('C')),
+                  height: MediaQuery.of(context).size.height/1.7,
+                  width: MediaQuery.of(context).size.width/3.35
+                ),
+              ],
             ),
           ],
         ),
