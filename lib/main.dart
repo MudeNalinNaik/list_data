a75fb1d0a10053030f889b6e83978694496817d9
NalinNaik <nalinnaikmude@gmail.com>
2024-04-18 09:07:13 +0530

    media query_column_testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,9 +7,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
       home: HomePage(),
     );
   }
@@ -21,36 +18,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              color: Colors.grey,
-              child: Center(child: Text('A')),
-              height: MediaQuery.of(context).size.height/3.2,
-              width: MediaQuery.of(context).size.width,
-            ),
-            SizedBox(height: 10,),
-            Row(
-              children: [
-                Container(
-                  color: Colors.grey,
-                  child: Center(child: Text('B')),
-                  height: MediaQuery.of(context).size.height/1.7,
-                  width: MediaQuery.of(context).size.width/1.5,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  color: Colors.grey,
-                  child: Center(child: Text('C')),
-                  height: MediaQuery.of(context).size.height/1.7,
-                  width: MediaQuery.of(context).size.width/3.35
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            color: Colors.red,
+            child: Center(child: Text('A')),
+            height: MediaQuery.of(context).size.height/3.2,
+            width: 200
+          ),
+          SizedBox(height: 10,),
+          Container(
+            color: Colors.pink,
+            child: Center(child: Text('B')),
+            height: MediaQuery.of(context).size.height/3.2,
+            width: MediaQuery.of(context).size.width/2,
+          ),
+          SizedBox(height: 10,),
+          Container(
+            color: Colors.yellow,
+            child: Center(child: Text('C')),
+            height: MediaQuery.of(context).size.height/3.2,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ],
       ),
     );
   }
