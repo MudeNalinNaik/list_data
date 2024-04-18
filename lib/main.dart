659e52215f5e0e5a941b42f6f7c4dea2bc01cfaa
NalinNaik <nalinnaikmude@gmail.com>
2024-04-18 09:21:39 +0530

    container_border

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,26 +17,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(crossAxisAlignment: CrossAxisAlignment.end,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Container(
             color: Colors.red,
             child: Center(child: Text('A')),
-            height: MediaQuery.of(context).size.height/3.2,
-            width: 200
+            height: MediaQuery.of(context).size.height / 3.2,
+            width: MediaQuery.of(context).size.width / 4,
           ),
-          SizedBox(height: 10,),
-          Container(
-            color: Colors.pink,
-            child: Center(child: Text('B')),
-            height: MediaQuery.of(context).size.height/3.2,
-            width: MediaQuery.of(context).size.width/2,
+          SizedBox(
+            height: 10,
           ),
-          SizedBox(height: 10,),
+          Row(mainAxisAlignment:  MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                child: Center(child: Text('B1')),
+                height: MediaQuery.of(context).size.height / 3.2,
+                width: MediaQuery.of(context).size.width / 3.2,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.green,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(320))),
+              ),
+              Container(
+                child: Center(child: Text('B2')),
+                height: MediaQuery.of(context).size.height / 3.2,
+                width: MediaQuery.of(context).size.width / 3.2,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.green,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
           Container(
             color: Colors.yellow,
             child: Center(child: Text('C')),
-            height: MediaQuery.of(context).size.height/3.2,
+            height: MediaQuery.of(context).size.height / 3.2,
             width: MediaQuery.of(context).size.width,
           ),
         ],
