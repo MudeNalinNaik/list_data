08b4d1b16d4c7d989b0e60ad455349bb52c9fae1
NalinNaik <nalinnaikmude@gmail.com>
2024-04-27 08:22:28 +0530

    Text colours

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int text = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Container(
-            color: Colors.red,
-            child: Center(child: Text('A')),
-            height: MediaQuery.of(context).size.height / 3.2,
-            width: MediaQuery.of(context).size.width / 4,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(mainAxisAlignment:  MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                child: Center(child: Text('B1')),
-                height: MediaQuery.of(context).size.height / 3.2,
-                width: MediaQuery.of(context).size.width / 3.2,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.green,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(320))),
-              ),
-              Container(
-                child: Center(child: Text('B2')),
-                height: MediaQuery.of(context).size.height / 3.2,
-                width: MediaQuery.of(context).size.width / 3.2,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.green,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            color: Colors.yellow,
-            child: Center(child: Text('C')),
-            height: MediaQuery.of(context).size.height / 3.2,
+            height: 200,
             width: MediaQuery.of(context).size.width,
+            color: Colors.green,
           ),
+          const SizedBox(height: 20),
+          // Adds space between Container and other items
+          const Text(
+            ' Item 1',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.lime),
+          ),
+          const Text(
+            ' Item 2',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.purple),
+          ),
+          const Text(
+            ' Item 3',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+// Add more widgets here as needed
         ],
       ),
     );
